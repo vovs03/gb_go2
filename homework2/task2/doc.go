24e1d46433c15a6a5a8fa27f0db6eebfb509e6fd
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2021-05-27 18:47:28 +0300

    Add: create package doc | doc.go - func return string

---
+++ homework2/task2/doc.go
@@ -0,0 +1,13 @@
+// Package errors implements functions to demonstrate godoc features
+//
+// The MyFunction returns some string
+//
+// MyFunction() string
+//
+// So, you are allowed to call this function and get some string
+package doc
+
+// MyFunction returns a string
+func MyFunction() string {
+	return "some result"
+}
